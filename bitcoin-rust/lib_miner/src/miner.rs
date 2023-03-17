e9aeeac3651a985bfe2565912544962bc5f8933a
Nigel Chen Chin Hao <nigel_ncch@hotmail.com>
2023-03-17 22:41:45 +0800

    lib_miner WIP

--- bitcoin-rust/lib_miner/src/miner.rs
+++ bitcoin-rust/lib_miner/src/miner.rs
@@ -1,19 +1,23 @@
-// This file is part of the project for the module CS3235 by Prateek 
+// This file is part of the project for the module CS3235 by Prateek
 // Copyright 2023 Ruishi Li, Bo Wang, and Prateek Saxena.
 // Please do not distribute.
 
-// This file implements the Miner struct and related methods. 
+// This file implements the Miner struct and related methods.
 // The miner has one key task: to solve a given puzzle (a string) with specified number of threads and difficulty levels.
 // You can see detailed instructions in the comments below.
 // You can also look at the unit tests in ./lib.rs to understand the expected behavior of the miner.
 
-use std::{thread, convert};
+use hex;
+use rand::{
+    distributions::{Alphanumeric, DistString},
+    Rng, SeedableRng,
+};
+use rand_pcg::Pcg32;
+use sha2::{Digest, Sha256};
+
 use std::collections::BTreeMap;
 use std::sync::{Arc, Mutex, RwLock};
-use rand_pcg::Pcg32;
-use rand::{Rng, SeedableRng, distributions::{Alphanumeric, DistString}};
-use sha2::{Sha256, Digest};
-
+use std::{convert, thread};
 
 // A miner that solve puzzles.
 pub struct Miner {
@@ -26,7 +30,7 @@ pub struct Miner {
     leading_zero_len: u16,
 
     /// whether the miner is running or not
-    is_running: bool
+    is_running: bool,
 }
 
 type BlockId = String;
@@ -38,19 +42,19 @@ pub struct PuzzleSolution {
     /// the nonce string that should be prepended to the puzzle string for computing the hash
     pub nonce: String,
     /// the sha256 hash of (nonce || puzzle) in hex format
-    pub hash: BlockId
+    pub hash: BlockId,
 }
 
 impl Miner {
     // constructor
-    pub fn new () -> Miner {
-        Miner { 
+    pub fn new() -> Miner {
+        Miner {
             thread_count: 0,
             leading_zero_len: 0,
-            is_running: false
+            is_running: false,
         }
     }
-    
+
     /// The method to solve a puzzle with specified number of threads and difficulty levels.
     /// This method is a function on the class (without `self` as the 1st argument). The first parameter is a smart pointer to a miner instance.
     /// - `miner_p`: the smart pointer to the miner instance
@@ -61,26 +65,72 @@ impl Miner {
     /// - `thread_0_seed`: the seed for the random number generator for the first thread. The seed for the second thread should be `thread_0_seed + 1`, and so on.
     /// - `cancellation_token`: a smart pointer to a boolean value. If the value is set to true, all threads should stop even if they have not found a solution.
     /// - return: an optional value with the solution if the puzzle is solved, or None if the puzzle is cancelled.
-    pub fn solve_puzzle(miner_p: Arc<Mutex<Miner>>, puzzle: String, nonce_len: u16, leading_zero_len: u16, thread_count: u16, thread_0_seed: u64, cancellation_token: Arc<RwLock<bool>>) -> Option<PuzzleSolution> {
-        
+    pub fn solve_puzzle(
+        miner_p: Arc<Mutex<Miner>>,
+        puzzle: String,
+        nonce_len: u16,
+        leading_zero_len: u16,
+        thread_count: u16,
+        thread_0_seed: u64,
+        cancellation_token: Arc<RwLock<bool>>,
+    ) -> Option<PuzzleSolution> {
         // Please fill in the blank
         // In this function, you are expected to start multiple threads for solving the puzzle.
         // The threads should be spawned and joined in this function.
         // If any of the threads finds a solution, other threads should stop.
         // Additionally, if the cancellation_token is set to true, all threads should stop.
         // The purpose of the cancellation_token is to allow the miner to stop the computation when other nodes have already solved the exact same puzzle.
+
+        println!("The value of puzzle is {}", puzzle);
+        println!("The value of nonce_len is {}", nonce_len);
+        println!("The value of leading_zero_len is {}", leading_zero_len);
+        println!("The value of thread_count is {}", thread_count);
+        println!("The value of thread_0_seed is {}", thread_0_seed);
+
+        let mut threads = Vec::new();
+
+        for thread_id in 0..thread_count {
+            let puzzle = puzzle.clone();
+            let thread = thread::spawn(move || loop {
+                let thread_rng = Pcg32::from_entropy();
+                let nonce: String = thread_rng
+                    .sample_iter(&Alphanumeric)
+                    .take(nonce_len as usize)
+                    .map(char::from)
+                    .collect();
+
+                let challenge = format!("{}{}", nonce, puzzle);
+                let mut hash = Sha256::new();
+                hash.update(challenge);
+
+                let answer = hex::encode(hash.finalize());
+
+                if answer.starts_with(&"0".repeat(leading_zero_len as usize)) {
+                    let ps = PuzzleSolution {
+                        puzzle: puzzle.clone(),
+                        nonce: nonce,
+                        hash: answer,
+                    };
+                    println!(
+                        "Thread ID: {}, Puzzle: {}, Nonce: {}, Hash: {}",
+                        thread_id, ps.puzzle, ps.nonce, ps.hash
+                    );
+                }
+            });
+
+            threads.push(thread);
+        }
+
+        loop {}
+
         todo!();
-        
-    } 
-    
+    }
+
     /// Get status information of the miner for debug printing.
     pub fn get_status(&self) -> BTreeMap<String, String> {
         // Please fill in the blank
-        // For debugging purpose, you can return any dictionary of strings as the status of the miner. 
+        // For debugging purpose, you can return any dictionary of strings as the status of the miner.
         // It should be displayed in the Client UI eventually.
         todo!();
-        
     }
 }
-
-
