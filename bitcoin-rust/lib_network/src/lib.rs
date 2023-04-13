5a5d12ecb11da5d9cb4aff13a1ef38c4953f0a41
Nigel Chen Chin Hao <nigel_ncch@hotmail.com>
2023-04-14 01:36:32 +0800

    lib_network complete

--- bitcoin-rust/lib_network/src/lib.rs
+++ bitcoin-rust/lib_network/src/lib.rs
@@ -95,8 +95,9 @@ mod tests {
             loop {
                 let mut buf = String::new();
                 let len = reader.read_line(&mut buf).unwrap();
+
                 if len > 0 {
-                    print!("{}", "[handle_client] [Read] ".to_owned() + &buf);
+                    println!("{}", "[handle_client] [Read] ".to_owned() + &buf);
                 } else {
                     break;
                 }
